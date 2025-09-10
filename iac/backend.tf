5507f1634a2b5cec6efb0acfcc152fa6efc5bf4d
youssefadelG <youssefadel99@gmail.com>
2025-09-10 15:46:22 +0300

    [presentation] updated S3 bucket name

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-hello-487692781227"
+    bucket         = "tf-state-hello-695454131301"
     key            = "terraform.tfstate"
     region        = "us-east-1"
   }
